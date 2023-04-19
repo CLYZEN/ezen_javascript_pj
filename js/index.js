bb6d3c6785adbcaad2e40b41d28b037b639a528f
CLYZEN <jimuny01@gmail.com>
2023-04-19 18:29:33 +0900

    #2 fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 function getPopularMoive() {
   var temp = {}
+  var movieID = {}
   var movieUrlAPI =
     "https://api.themoviedb.org/3/movie/popular?api_key=8e7d2baf58a031cf34c92d428e1f6df7&language=ko-KR&page=1&region=KR"
+  var trailerKey = {}
 
   $.ajax({
     type: "GET",
@@ -9,20 +11,45 @@ function getPopularMoive() {
     dataType: "json",
     async: false,
     success: function (data) {
-      console.log(data)
       var results = data.results
-      console.log(results)
 
       for (const result of results) {
-        console.log(result)
-        console.log(result.title)
-        console.log(result.poster_path)
+        var movieId = result.id
+        function getMovieVideo() {
+          var VideokeyURL = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=8e7d2baf58a031cf34c92d428e1f6df7&language=ko-KR`
+          $.ajax({
+            type: "GET",
+            url: VideokeyURL,
+            dataType: "json",
+            async: false,
+            success: function (data) {
+              var results = data.results
 
-        let imgURL = "https://image.tmdb.org/t/p/w500" + result.poster_path
-        $(".images").append(
-          `<a href = "./detail.html?id=${result.id}"><img src="${imgURL}"></a>`
-        )
-        $(".images").append(`제목 : ${result.title},`)
+              for (const result of results) {
+                let videoURL = "https://www.youtube.com/embed/" + result.key
+                console.log(videoURL)
+                trailerKey.url = videoURL
+                for (var i = 0; i >= 10; i++) {
+                  $(".carousel-item")
+                    .append(`                  <iframe width="100%" height="56.25%"
+                  src="${videoURL}"
+                  title="YouTube video player" frameborder="0"
+                  calss="d-block w-100"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen></iframe>`)
+                }
+              }
+
+              // $(".images").append(`제목 : ${result.title},`)
+            },
+            error: function (request, status, error) {
+              console.log("code:" + request.status)
+              console.log("message:" + request.responseText)
+              console.log("error:" + error)
+            },
+          })
+        }
+        getMovieVideo()
       }
     },
     error: function (request, status, error) {
@@ -33,5 +60,6 @@ function getPopularMoive() {
   })
   return temp
 }
+//api.themoviedb.org/3/movie/{movie_id}/videos?api_key=8e7d2baf58a031cf34c92d428e1f6df7&language=ko-KR
 
 getPopularMoive()
