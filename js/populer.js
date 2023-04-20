893d35f207a6aff41f2d2da9c611d3c3bcc08514
“hanzoeungit config --global user.email “dorlgodfhd@naver.com <dorlgodfhd@naver.comgit config --listgit config --global user.name hanzoeun>
2023-04-20 13:07:01 +0900

    인기영화

--- js/populer.js
+++ js/populer.js
@@ -14,7 +14,7 @@ function getPopularMoive() {
       // console.log(results);
 
       for (const result of results) {
-        // console.log(result);
+        console.log(result);
         // console.log(result.title);
         // console.log(result.poster_path);
         let imgURL = "https://image.tmdb.org/t/p/w300" + result.poster_path;
@@ -22,10 +22,14 @@ function getPopularMoive() {
         // img
         $(".images").append(
           `<div class="movie-img"><a href = "./detail.html?id=${result.id}"><img src="${imgURL}"></a>
-          <div class="movie-title"><p>${result.title}</p></div>
+          <div class="movie-title"><p>${result.title}<br>개봉일:${result.release_date}</p></div>
           </div>
           `
         );
+
+        $(function () {
+          $(".images").on("mouseover focus", function () {});
+        });
 
         // $(".movie-img").append(`<div class="contents"><p >제목</p></div>`);
       }
