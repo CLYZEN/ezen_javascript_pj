a574613e1b9a3152f384750e653b22f81bfa8b05
“hanzoeungit config --global user.email “dorlgodfhd@naver.com <dorlgodfhd@naver.comgit config --listgit config --global user.name hanzoeun>
2023-04-20 11:34:11 +0900

    인기영화

---
+++ js/populer.js
@@ -0,0 +1,42 @@
+function getPopularMoive() {
+  var temp = {};
+  var movieUrlAPI =
+    "https://api.themoviedb.org/3/movie/popular?api_key=8e7d2baf58a031cf34c92d428e1f6df7&language=ko-KR&page=1&region=KR";
+
+  $.ajax({
+    type: "GET",
+    url: movieUrlAPI,
+    dataType: "json",
+    async: false,
+    success: function (data) {
+      // console.log(data);
+      var results = data.results;
+      // console.log(results);
+
+      for (const result of results) {
+        // console.log(result);
+        // console.log(result.title);
+        // console.log(result.poster_path);
+        let imgURL = "https://image.tmdb.org/t/p/w300" + result.poster_path;
+
+        // img
+        $(".images").append(
+          `<div class="movie-img"><a href = "./detail.html?id=${result.id}"><img src="${imgURL}"></a>
+          <div class="movie-title"><p>${result.title}</p></div>
+          </div>
+          `
+        );
+
+        // $(".movie-img").append(`<div class="contents"><p >제목</p></div>`);
+      }
+    },
+    error: function (request, status, error) {
+      console.log("code:" + request.status);
+      console.log("message:" + request.responseText);
+      console.log("error:" + error);
+    },
+  });
+  return temp;
+}
+
+getPopularMoive();
