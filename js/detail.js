b0dcb17e162ce87f9aa34c2b6f809c9d032e34ff
github jyong0000 <github jyong3836@gmail.com>
2023-04-20 17:45:09 +0900

    detail

--- js/detail.js
+++ js/detail.js
@@ -6,19 +6,16 @@ console.log(currentURL);
 const urlObj = new URL(currentURL);
 const params = urlObj.searchParams;
 const id = params.get("id");
-console.log(id);
 
 function getMoiveDetail() {
   var temp = {};
   const movieUrlAPI = `https://api.themoviedb.org/3/movie/${id}?api_key=d5973569877cd7950c8f05e4748588b8&language=ko-KR`;
-
   $.ajax({
     type: "GET",
     url: movieUrlAPI,
     dataType: "json",
     async: false,
     success: function (data) {
-      console.log("전체 data : ");
       console.log(data);
 
       let imgURL = "https://image.tmdb.org/t/p/w500" + data.poster_path;
@@ -28,13 +25,14 @@ function getMoiveDetail() {
       const title = data.title;
       const language = data.original_language;
       const release_date = data.release_date;
+      const tagline = data.tagline;
       const overview = data.overview;
       const vote_average = data.vote_average;
       const vote_count = data.vote_count;
 
       $(".title").text(`${title}`);
       $(".language").text(`개봉일(언어) :  ${release_date} (${language})`);
-
+      $(".tagline").text(`${tagline}`);
       $(".overview").text(`${overview}`);
       $(".vote_average").text(
         `평점 : ${vote_average} / 투표인원 : ${vote_count} `
